85d6854063c3a745373d25797dbdb8d99a26b835
darrylbalderas <drrylbalderas@gmail.com>
2018-11-28 22:38:26 -0800

    REF: Make pylint updates
    Signed-off-by: darrylbalderas <drrylbalderas@gmail.com>

--- src/wingstop.py
+++ src/wingstop.py
@@ -3,12 +3,14 @@ import requests
 import pandas
 import os
 
+
 def clear_csv_file(filename):
     """
     Removes csv file
     """
-    if(os.path.exists(filename)):
+    if os.path.exists(filename):
         os.remove(filename)
+
 
 def get_wingstop_state_locations(location_url):
     """
@@ -16,8 +18,14 @@ def get_wingstop_state_locations(location_url):
     """
     locations_page = requests.get(location_url)
     soup_object = BeautifulSoup(locations_page.text, 'html.parser')
-    participating_states = soup_object.find(id="ParticipatingStates").find_all('a')
-    return [participating_state['href'].split('/')[2] for participating_state in participating_states]
+    participating_states = soup_object.find(id="ParticipatingStates")
+    state_anchor_tags = participating_states.find_all('a')
+    return [extract_location(tag) for tag in state_anchor_tags]
+
+
+def extract_location(state_anchor_tag):
+    return state_anchor_tag['href'].split('/')[2]
+
 
 def get_location_list(location_url, state_name):
     """
@@ -25,14 +33,15 @@ def get_location_list(location_url, state_name):
     """
     page = requests.get(location_url + '/' + state_name)
     soup = BeautifulSoup(page.text, 'html.parser')
-    locations = soup.find(id="ParticipatingRestaurants").find_all(class_="streetaddress adr")
+    restaurants = soup.find(id="ParticipatingRestaurants")
+    locations = restaurants.find_all(class_="streetaddress adr")
     state_information = dict()
     for index, location in enumerate(locations):
         state_information[index] = dict()
-        location_address = location.find_all('span')
-        state_information[index]['address'] = location_address[0].contents[0].strip()
-        state_information[index]['city'] = location_address[1].contents[0].strip()
-        state_information[index]['state'] = location_address[2].contents[0].strip()
+        address = location.find_all('span')
+        state_information[index]['address'] = address[0].contents[0].strip()
+        state_information[index]['city'] = address[1].contents[0].strip()
+        state_information[index]['state'] = address[2].contents[0].strip()
     return pandas.DataFrame(state_information).transpose()
 
 
@@ -41,28 +50,29 @@ def get_latitude_longitude(address, city, state):
     split_address = '+'.join(address.split(" "))
     split_city = '+'.join(city.split(" "))
     split_state = '+'.join(state.split(" "))
-    string = mapping_url + split_address + ",+" + split_city + ",+" + split_state
+    string = mapping_url + split_address + ",+" \
+        + split_city + ",+" + split_state
     response = requests.get(string).json()
     try:
         location = response['results'][0]['geometry']['location']
         return location['lat'], location['lng']
-    except:
+    except IndexError:
         return None, None
 
 
-def create_list_of_locations(state_informations):
+def create_list_of_locations(state_info):
     """
     Writes to a specified csv file
     """
     df = pandas.DataFrame(columns=['latitude', 'longitude'])
-    for key in state_informations.index:
-        lat, lng = get_latitude_longitude(
-            state_informations.iloc[key]['address'], state_informations.iloc[key]['city'], state_informations.iloc[key]['state'])
-        if lat == None and lng == None:
-            pass
-        else: 
+    for key in state_info.index:
+        lat, lng = get_latitude_longitude(state_info.iloc[key]['address'],
+                                          state_info.iloc[key]['city'],
+                                          state_info.iloc[key]['state'])
+        if lat and lng:
             df.loc[key] = [lat, lng]
     return df
+
 
 def main():
     """
@@ -79,6 +89,7 @@ def main():
         df = pandas.concat([df, create_list_of_locations(state_information)])
     print('Finish with get coordinates')
     df.to_csv('coordinates.csv', index=False)
+
 
 if __name__ == "__main__":
     main()
